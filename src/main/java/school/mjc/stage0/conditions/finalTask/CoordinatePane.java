b400243ea1071b436fe75c78898b66dbea872348
Akmal Ismoilov <master80098@gmail.com>
2022-07-28 15:34:25 +0500

    Final task completed!

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if(x == 0 && y == 0)
+        {
+            System.out.println("zero");
+            return;
+        }
+        boolean posX = x > 0, posY = y > 0;
+        if(posX)
+        {
+            if(posY)
+                System.out.println("first");
+            else
+                System.out.println("fourth");
+        }
+        else
+        {
+            if(posY)
+                System.out.println("second");
+            else
+                System.out.println("third");
+        }
     }
 }
